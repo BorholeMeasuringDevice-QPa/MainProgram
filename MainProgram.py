b03862f8a1b86e7d0d2f52d85e3f51eb70009d0c
Paweł Kłósko <kloskopawel@gmail.com>
2018-06-26 14:16:33 +0200

    Initial commit

---
+++ MainProgram.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Program do wczytywania i obsługi danych z Arduino
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
+
+# funkcja odczytująca dane z Arduino
+def data_reader():
+    return True
+
+# funckja formatująca dane na typy obsługiwane przez program
+def data_converter():
+    return True
+
+#funkcja zapisująca odczyty do pliku
+def save_to_file():
+    return True
+
+#funkcja uśredniająca dane
+def data_averaging():
+    return True
+
+#funckja wyświetlająca dane
+def data_viewer():
+    return True

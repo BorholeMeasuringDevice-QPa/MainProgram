496e2c99dd6583685ec2291dc6079be0fbc78f27
Paweł Kłósko <kloskopawel@gmail.com>
2018-06-26 15:26:06 +0200

    Added random data generating

--- MainProgram.py
+++ MainProgram.py
@@ -3,6 +3,29 @@
 #Program do wczytywania i obsługi danych z Arduino
 
 import datetime
+import random
+
+#generator losowych danych
+def raw_data_generator():
+    raw_data={
+    "magnetometr_x":random.uniform(30.0,40.0),
+    "magnetometr_y":random.uniform(20.0,30.0),
+    "magnetometr_z":random.uniform(10.0,20.0),
+    "akcelerometr_x":random.uniform(40.0,50.0),
+    "akcelerometr_y":random.uniform(50.0,60.0),
+    "akcelerometr_z":random.uniform(60.0,70.0),
+    "barometr_t":random.uniform(20.0,30.0),
+    "barometr_p":random.uniform(1000.0,1100.0),
+    "barometr_h":random.uniform(100.0,101.0)}
+    return raw_data
+
+# funkcja zbierająca odczyty do dalszych obliczeń
+
+def data_collector():
+    Data_collection = [];
+    for i in range(10):
+        Data_collection.insert(i,raw_data_generator());
+    return Data_collection
 
 # funkcja odczytująca dane z Arduino
 def data_reader():
@@ -39,10 +62,8 @@ def data_viewer():
     return True
 
 def main(args):
-
-    data=[2.35,2.47,3.01,2.5,2.7,3.0,2.12]
-    save_to_file("TESTOWY NAPIS")
-    print(data_averaging(data))
+    for e in data_collector():
+        print(e)
     return 0
 
 if __name__ == '__main__':

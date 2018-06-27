0bcf7db4901d25951761a44d85f149f0e155099e
Paweł Kłósko <kloskopawel@gmail.com>
2018-06-27 12:28:24 +0200

    Added lised data to random data generator

--- RandomDataGenerator.py
+++ RandomDataGenerator.py
@@ -14,3 +14,17 @@ def raw_data_generator():
     "barometr_h":random.uniform(100.0,101.0),
     "enkoder_m": random.uniform(100.0, 101.0)}
     return raw_data
+
+def listed_data_generator():
+    data_list=[random.uniform(30.0,40.0),random.uniform(20.0,30.0),random.uniform(10.0,20.0),
+               random.uniform(40.0, 50.0),random.uniform(50.0,60.0),random.uniform(60.0,70.0),
+               random.uniform(20.0, 30.0),random.uniform(1000.0,1100.0),random.uniform(100.0,101.0),
+               random.uniform(100.0, 101.0)]
+    raw_data=""
+    for e in data_list:
+        raw_data+="|"+str(e)
+
+    return raw_data
+
+
+
